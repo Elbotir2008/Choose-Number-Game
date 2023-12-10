871dfd7576b774178e43afa93ace7caabfbf3d72
Asadov Elbotir <elbotir2008@gmail.com>
2023-12-10 20:29:46 +0500

    the end

---
+++ main.js
@@ -0,0 +1,23 @@
+let input = document.getElementById("input");
+let randomNumbers = Math.floor(Math.random() * 20) + 1;
+let heartCount = 0;
+localStorage.setItem("randomNumbers", randomNumbers);
+function handleSubmit() {
+  if (input.value == randomNumbers) {
+    alert("You Find Random Number, You Win!");
+  } else if (input.value == "") {
+    alert("You have to write any number from 1 to 20");
+  } else {
+    heartCount++;
+    input.value = "";
+    alert(`You did not find the given number, try again.`);
+  }
+
+  if (heartCount >= 5) {
+    if (alert("Your heart is done, Repait Game!")) {
+      location.reload();
+    } else {
+      location.reload();
+    }
+  }
+}
